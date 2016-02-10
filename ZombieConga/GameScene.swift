5b15bd055feb4575ccd8888f8b138abe5e05e0dc
Daniel Norton <daniel.norton@brimstead.com>
2016-02-10 00:00:02 -0600

    ch2. Iteration 3: Moving toward touches

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,7 +11,7 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    var firstZombie: SKSpriteNode?
+    var zombie = SKSpriteNode(imageNamed: "zombie1")
     var lastUpdateTime: NSTimeInterval = 0
     var updateInterval : NSTimeInterval = 0
     var zombieMovePointsPerSec: CGFloat = 480.0
@@ -22,23 +22,29 @@ class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
     
         drawBackground()
-        firstZombie = drawFirstZombie()
+        drawZombie()
     }
     
     override func update(currentTime: NSTimeInterval) {
         
-        if let zombie = firstZombie {
-
-            let v = CGPoint(x: zombieMovePointsPerSec, y: 0)
-            moveSprite(zombie, byVelocity: v)
-        }
-
+        moveSprite(zombie, byVelocity: velocity)
+        
         updateInterval = lastUpdateTime > 0
             ? currentTime - lastUpdateTime
             : 0
         
         lastUpdateTime = currentTime
         print("\(updateInterval*1000) milliseconds since last update")
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+
+        sceneTouched(touches)
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        sceneTouched(touches)
     }
     
     // MARK: - GameScene
@@ -52,7 +58,7 @@ class GameScene: SKScene {
         addChild(background)
     }
     
-    func drawFirstZombie() -> SKSpriteNode {
+    func drawZombie() -> SKSpriteNode {
         
         let zombie = SKSpriteNode(imageNamed: "zombie1")
         zombie.anchorPoint = CGPoint.zero
@@ -73,5 +79,29 @@ class GameScene: SKScene {
         let px = sprite.position.x + amountToMove.x
         let py = sprite.position.y + amountToMove.y
         sprite.position = CGPoint(x: px, y: py)
+    }
+    
+    func moveSprite(sprite: SKSpriteNode, toPoint point: CGPoint, pointsPerSecond points : CGFloat) {
+        
+        let ox = point.x - sprite.position.x
+        let oy = point.y - sprite.position.y
+        let offset = CGPoint(x: ox, y: oy)
+        
+        let length = CGFloat(sqrt(Double(offset.x * offset.x + offset.y * offset.y)))
+        
+        let dx = offset.x / length
+        let dy = offset.y / length
+        let direction = CGPoint(x: dx, y: dy)
+        
+        let vx = direction.x * points
+        let vy = direction.y * points
+        velocity = CGPoint(x: vx, y: vy)
+    }
+    
+    func sceneTouched(touches: Set<UITouch>) {
+        
+        guard let touch = touches.first else { return }
+        let touchLocation = touch.locationInNode(self)
+        moveSprite(zombie, toPoint: touchLocation, pointsPerSecond: zombieMovePointsPerSec)
     }
 }
