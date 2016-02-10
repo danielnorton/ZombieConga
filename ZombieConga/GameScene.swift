7f5322c1295ad7b0f60fefde48c12834c5facbfb
Daniel Norton <daniel.norton@brimstead.com>
2016-02-09 19:15:18 -0600

    ch1. Challenges

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,13 +11,31 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    
+    // MARK: - SKScene
     override func didMoveToView(view: SKView) {
     
+        drawBackground()
+        drawFirstZombie()
+    }
+    
+    // MARK: - GameScene
+    func drawBackground() {
+        
         backgroundColor = SKColor.blackColor()
         
         let background = SKSpriteNode(imageNamed: "background1")
         background.anchorPoint = CGPoint.zero
         background.zPosition = -1
         addChild(background)
+    }
+    
+    func drawFirstZombie() {
+        
+        let zombie = SKSpriteNode(imageNamed: "zombie1")
+        zombie.anchorPoint = CGPoint.zero
+        zombie.position = CGPoint(x: 400, y: 400)
+        zombie.setScale(2.0)
+        addChild(zombie)
     }
 }
